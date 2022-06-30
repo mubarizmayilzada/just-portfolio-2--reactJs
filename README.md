9796180bd8b02a660549e6cbf591f63059683311
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-01 03:12:32 +0400

    initial commit - connected with tailwindCSS

--- README.md
+++
@@ -1,2 +0,0 @@
-# just-portfolio-2--reactJs
-the React portfolio website with Tailwind CSS - Dark Mode . Actually I created that with Vite.Js
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className='uppercase text-5xl text-cyan-400'>App</h1>
+  )
+}
+
+export default App
