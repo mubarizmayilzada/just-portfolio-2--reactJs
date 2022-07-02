ae48fde50c10fac32ce346aa17c5085b1224bf64
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-02 04:04:49 +0400

    Hero section and dark mode is already done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
+
+  const [theme, setTheme] = useState(null);
+
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+  useEffect(()=>{
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    }
+    else{
+      setTheme('light');
+    }
+  },[]);
+
+  useEffect(()=>{
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    }
+    else{
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme])
+
   return (
-    <h1 className='uppercase text-5xl text-cyan-400'>App</h1>
-  )
+      <>
+        <button type='button' onClick={handleThemeSwitch} className='fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md'>
+          {theme === 'dark' ? '🌞' : '🌙'}
+        </button>
+            <div className="font-inter bg:white dark:bg-slate-900">
+              <HeroSection/>
+        
+            </div>
+      </>
+    )
 }
 
 export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div className='flex items-center justify-center flex-col py-20'>
+        <div className='text-center'>
+            <h1 className='text-2xl md:text-4xl mb-1 md:mb-3 text-indigo-600 font-semibold dark:text-indigo-500'>
+                Hi, this is Mubariz
+            </h1>
+            <p className='text-md md:text-xl max-w-md mb-3 text-gray-600 dark:text-gray-300'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, officia!
+            </p>
+            <a href="#" className='inline-block px-8 py-3 border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md'>See Works</a>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection;
